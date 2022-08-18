aff63b0e0e3b549d138ce769f810bd4a7e411a87
bitcamp <bitcamp@DESKTOP-3JTPOOU>
2022-08-18 20:29:10 +0900

    Commit : 2022.08.18 퇴근!

--- 08.Model2MVCShop(RestFul Client)/src/client/app/RestHttpClientApp.java
+++ 08.Model2MVCShop(RestFul Client)/src/client/app/RestHttpClientApp.java
@@ -16,6 +16,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.domain.Product;
 
 
 
@@ -48,7 +49,13 @@ public class RestHttpClientApp {
 //		RestHttpClientApp.addUserTest_Codehaus();
 		
 //		System.out.println("\n====================================\n");
-		RestHttpClientApp.updateUserTest_Codehaus();
+//		RestHttpClientApp.updateUserTest_Codehaus();
+		
+//		System.out.println("\n====================================\n");
+//		RestHttpClientApp.getProductTest_Codehaus();
+		
+//		System.out.println("\n====================================\n");
+		RestHttpClientApp.addProductTest_Codehaus();
 	}
 	
 	
@@ -120,7 +127,7 @@ public class RestHttpClientApp {
 		//==> 다른 방법으로 serverData 처리 
 		//System.out.println("[ Server 에서 받은 Data 확인 ] ");
 		//String serverData = br.readLine();
-		//System.out.println(serverData);
+//		System.out.println(serverData);
 		
 		//==> API 확인 : Stream 객체를 직접 전달 
 		JSONObject jsonobj = (JSONObject)JSONValue.parse(br);
@@ -326,5 +333,96 @@ public class RestHttpClientApp {
 		User user01 = objectMapper01.readValue(jsonObj.toString(), User.class);
 		System.out.println(user01);
 	}
+	
+	public static void getProductTest_Codehaus() throws Exception{
+		
+		// HttpClient : Http Protocol 의 client 추상화 
+		HttpClient httpClient = new DefaultHttpClient();
+		
+		String url= 	"http://127.0.0.1:8080/product/json/getProduct/10001";
+
+		// HttpGet : Http Protocol 의 GET 방식 Request
+		HttpGet httpGet = new HttpGet(url);
+		httpGet.setHeader("Accept", "application/json");
+		httpGet.setHeader("Content-Type", "application/json");
+		
+		// HttpResponse : Http Protocol 응답 Message 추상화
+		HttpResponse httpResponse = httpClient.execute(httpGet);
+		
+		//==> Response 확인
+		System.out.println(httpResponse);
+		System.out.println();
+
+		//==> Response 중 entity(DATA) 확인
+		HttpEntity httpEntity = httpResponse.getEntity();
+		
+		//==> InputStream 생성
+		InputStream is = httpEntity.getContent();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+		
+		//==> 다른 방법으로 serverData 처리 
+		//System.out.println("[ Server 에서 받은 Data 확인 ] ");
+		//String serverData = br.readLine();
+		//System.out.println(serverData);
+		
+		//==> API 확인 : Stream 객체를 직접 전달 
+		JSONObject jsonobj = (JSONObject)JSONValue.parse(br);
+		System.out.println(jsonobj);
+	
+		ObjectMapper objectMapper = new ObjectMapper();
+//		 User user = objectMapper.readValue(jsonobj.toString(), User.class);
+		Product product = objectMapper.readValue(jsonobj.toString(), Product.class);
+		 System.out.println(product);
+	}
+	
+	public static void addProductTest_Codehaus() throws Exception {
+		
+		HttpClient httpClient = new DefaultHttpClient();
+		
+		String url = "http://127.0.0.1:8080/product/json/addProduct";
+		
+		HttpPost httpPost = new HttpPost(url);
+		httpPost.setHeader("Accept", "application/json");
+		httpPost.setHeader("Content-Type", "application/json");
+				
+//		user.setUserId("user77");
+//		user.setUserName("김아무개");
+//		user.setPassword("7777");
+		Product product = new Product();
+		product.setProdNo(Integer.parseInt("11111"));
+		product.setProdName("레스트 테스트 상품");
+		product.setPrice(Integer.parseInt("1111"));
+		product.setManuDate("2022.08.10");
+		product.setFileName("test product");
+		product.setProdDetail("test product detail");
+//		product.setRegDate("2011-10-07");
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		String jsonValue = objectMapper.writeValueAsString(product);
+
+		System.out.println("jsonValue : "+jsonValue);
+		System.out.println();
+		
+		HttpEntity httpEntity = new StringEntity(jsonValue,"utf-8");
+		
+		httpPost.setEntity(httpEntity);
+		HttpResponse httpResponse = httpClient.execute(httpPost);
+				
+		System.out.println();
+		System.out.println("httpResponse : "+httpResponse);
+		
+		HttpEntity httpEntity01 = httpResponse.getEntity();
+		
+		InputStream is = httpEntity01.getContent();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is,"utf-8"));
+		
+		JSONObject jsonObj = (JSONObject)JSONValue.parse(br);		
+		System.out.println("jsonObj : "+jsonObj);
+		
+		ObjectMapper objectMapper01 = new ObjectMapper();
+		Product product01 = objectMapper01.readValue(jsonObj.toString(), Product.class);
+		System.out.println(product01);
+	}	
 	
 }
